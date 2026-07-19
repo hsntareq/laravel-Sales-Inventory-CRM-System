4712511f8c1d3c28c6a4a6e26e6f0e64b32d251e
Hasan Tareq <hsntareq@gmail.com>
2026-07-19 17:33:28 +0600

    Remove temporary refactoring scripts

--- refactor_dashboard_overlap.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('resources/js/Pages/Dashboard.jsx', 'utf8');
-content = content.replace(/className="nexus-input pl-9/g, 'className="nexus-input !pl-9');
-fs.writeFileSync('resources/js/Pages/Dashboard.jsx', content);
-console.log("Fixed overlapping icon.");
--- refactor_dashboard_select.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('resources/js/Pages/Dashboard.jsx', 'utf8');
-content = content.replace(/<Select /g, '<Select menuPosition="fixed" menuPortalTarget={document.body} ');
-fs.writeFileSync('resources/js/Pages/Dashboard.jsx', content);
-console.log("Fixed Select components.");
